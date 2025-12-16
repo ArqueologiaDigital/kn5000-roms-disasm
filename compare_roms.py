2fe0f29e30ea609485237104651bbc401e16737e
Felipe Correa da Silva Sanches <juca@members.fsf.org>
2025-12-16 00:17:32 -0300

    compare_roms.py

---
+++ compare_roms.py
@@ -0,0 +1,17 @@
+original = open("kn5000_v10_program.rom", "rb").read()
+#rebuilt =  open("kn5000_v10_rebuilt.rom", "rb").read()
+rebuilt =  open("kn5000_v10_rebuilt.p", "rb").read()
+
+if len(rebuilt) > len(original):
+	print("Rebuilt ROM is too big!")
+
+score = 0
+for i in range(len(original)):
+	if i >= len(rebuilt):
+		print("Rebuilt ROM is too small")
+		break
+	if original[i] == rebuilt[i]:
+		score += 1
+
+print(f"Similarity: {score/len(original):0.8f}%")
+
