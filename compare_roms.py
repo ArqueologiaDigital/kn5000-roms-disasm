115a99ca7b4a94a9acd95ed346c1b3b456f9b579
Felipe Correa da Silva Sanches <juca@members.fsf.org>
2025-12-30 23:22:20 -0300

    additional rearrangement of the repo structure

--- compare_roms.py
+++ compare_roms.py
@@ -1,16 +1,24 @@
-original = open("original_ROMs/kn5000_v10_program.rom", "rb").read()
-rebuilt =  open("rebuilt/kn5000_v10_program.rebuilt.rom", "rb").read()
+import sys
 
-if len(rebuilt) > len(original):
-	print("Rebuilt ROM is too big!")
+print()
 
-score = 0
-for i in range(len(original)):
-	if i >= len(rebuilt):
-		print("Rebuilt ROM is too small")
-		break
-	if original[i] == rebuilt[i]:
-		score += 1
+for filename in ["kn5000_v10_program"]:
+	print(f" ==== {filename} ====")
+	original_name = f"original_ROMs/{filename}.rom"
+	rebuilt_name = f"rebuilt_ROMs/{filename}.rebuilt.rom"
+	original = open(original_name, "rb").read()
+	rebuilt =  open(rebuilt_name, "rb").read()
 
-print(f"Similarity: {100*score/len(original):0.2f}%  ({len(original)-score} incorrect bytes)")
+	if len(rebuilt) > len(original):
+		print("Rebuilt ROM is too big!")
+
+	score = 0
+	for i in range(len(original)):
+		if i >= len(rebuilt):
+			print("Rebuilt ROM is too small")
+			break
+		if original[i] == rebuilt[i]:
+			score += 1
+
+	print(f"Similarity: {100*score/len(original):0.2f}%  ({len(original)-score} incorrect bytes)\n\n")
 
