a6b4f9c2fe4e40587389a4e04645ee5d07d78d5d
Felipe Correa da Silva Sanches <juca@members.fsf.org>
2026-01-07 13:35:14 -0300

    romset bytematch: 48.77%  (3122540 incorrect bytes)
    ==== maincpu ====
    Similarity: 99.94%  (1310 incorrect bytes)
    
    ==== subcpu boot ====
    Rebuilt ROM is too small
    Similarity: 0.00%  (131072 incorrect bytes)
    
    ==== subcpu payload ====
    Similarity: 100.00%
    
    ==== table data ====
    Similarity: 32.42%  (1417294 incorrect bytes)
    
    ==== custom data ====
    Rebuilt ROM is too small
    Similarity: 0.00%  (1048576 incorrect bytes)
    
    ==== hdae5000 ====
    Rebuilt ROM is too small
    Similarity: 0.00%  (524288 incorrect bytes)

--- compare_roms.py
+++ compare_roms.py
@@ -1,20 +1,30 @@
+import os
 import sys
 
 print()
 
 results = {}
 msg = ""
+total_length = 0
+total_score = 0
+
 for entry in [
-	("maincpu", "kn5000_v10_program"),
-	("subcpu payload", "kn5000_subprogram_v142"),
-	("table data", "kn5000_table_data")
+	("maincpu",		"kn5000_v10_program.rom",	"kn5000_v10_program.rebuilt.rom"),
+	("subcpu boot",		"kn5000_subcpu_boot.ic30",	"kn5000_subcpu_boot.rebuilt.rom"),
+	("subcpu payload",	"kn5000_subprogram_v142.rom",	"kn5000_subprogram_v142.rebuilt.rom"),
+	("table data",		"kn5000_table_data.rom",	"kn5000_table_data.rebuilt.rom"),
+	("custom data",		"kn5000_custom_data.ic19",	"kn5000_custom_data.rebuilt.rom"),
+	("hdae5000",		"hd-ae5000_v2_06i.ic4",		"hd-ae5000_v2_06i.rebuilt.rom"),
 ]:
-	key, filename = entry
-	msg += f"==== {filename} ====\n"
-	original_name = f"original_ROMs/{filename}.rom"
-	rebuilt_name = f"rebuilt_ROMs/{filename}.rebuilt.rom"
+	key, original_name, rebuilt_name = entry
+	msg += f"==== {key} ====\n"
+	original_name = f"original_ROMs/{original_name}"
+	rebuilt_name = f"rebuilt_ROMs/{rebuilt_name}"
 	original = open(original_name, "rb").read()
-	rebuilt =  open(rebuilt_name, "rb").read()
+	try:
+		rebuilt =  open(rebuilt_name, "rb").read()
+	except FileNotFoundError:
+		rebuilt = []
 
 	if len(rebuilt) > len(original):
 		msg += "Rebuilt ROM is too big!\n"
@@ -29,19 +39,22 @@ for entry in [
 
 	percentage = f"{100*score/len(original):0.2f}%"
 	badbytes = len(original)-score
-	results[key] = (percentage, badbytes)
+
+	total_length += len(original)
+	total_score += score
 
 	if badbytes:
 		msg += f"Similarity: {percentage}  ({badbytes} incorrect bytes)\n\n"
 	else:
 		msg += f"Similarity: {percentage}\n\n"		
 
-title = []
-for key, value in results.items():
-	percentage, badbytes = value
-	if badbytes:
-		title.append(f"{key}: {percentage} ({badbytes} bad bytes)")
-	else:
-		title.append(f"{key}: {percentage}")
-print (" | ".join(title) + "\n")
+
+total_badbytes = total_length - total_score
+total_percentage = f"{100*total_score/total_length:0.2f}%"
+
+if total_badbytes:
+	print(f"romset bytematch: {total_percentage}  ({total_badbytes} incorrect bytes)\n")
+else:
+	print(f"romset bytematch: {total_percentage}\n")
+
 print (msg)
